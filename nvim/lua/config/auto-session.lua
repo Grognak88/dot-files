5f3ab6ad21e18bf416cc5ea991ed9de04057f935
Alexander Erlingsen <grognak88@gmail.com>
2024-06-12 23:08:48 +0200

    fix autosession

--- nvim/lua/config/auto-session.lua
+++ nvim/lua/config/auto-session.lua
@@ -1,8 +1,14 @@
 local auto_session = require("auto-session")
 
+local function change_nvim_tree_dir()
+	local nvim_tree = require("nvim-tree")
+	nvim_tree.change_dir(vim.fn.getcwd())
+end
+
 auto_session.setup({
-  auto_restore_enabled = false,
-  auto_session_supress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" }
+	auto_restore_enabled = false,
+	auto_session_supress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+	post_restore_cmds = { change_nvim_tree_dir, "NvimTreeOpen" },
 })
 
 local keymap = vim.keymap
